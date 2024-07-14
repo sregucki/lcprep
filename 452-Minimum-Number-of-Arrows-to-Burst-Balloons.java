1e8b152a32e31a4d1a0188144ba35c02b44e812b
Sebastian Regucki <sebastian.regucki@outlook.com>
2024-07-14 18:21:52 +0200

    [452] [Time Beats: 18.24%] [Memory Beats: 5.28%] - LeetPush

--- 452-Minimum-Number-of-Arrows-to-Burst-Balloons.java
+++ 452-Minimum-Number-of-Arrows-to-Burst-Balloons.java
@@ -1,18 +1,19 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        List<int[]> ans = new ArrayList<>();
+        int res = 1;
         Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
-        for (int i = 0; i < points.length; i++) {
+        int[] last = points[0];
+        for (int i = 1; i < points.length; i++) {
             int[] currInterval = points[i];
-            if (ans.isEmpty() || currInterval[0] > ans.get(ans.size() - 1)[1]) {
-                ans.add(currInterval);
+            if (currInterval[0] > last[1]) {
+                res++;
+                last = currInterval;
             } else {
-                int[] mergedInterval = new int[] { Math.max(currInterval[0], ans.get(ans.size() - 1)[0]),
-                        Math.min(currInterval[1], ans.get(ans.size() - 1)[1]) };
-                ans.remove(ans.size() - 1);
-                ans.add(mergedInterval);
+                int[] mergedInterval = new int[] { Math.max(currInterval[0], last[0]),
+                        Math.min(currInterval[1], last[1]) };
+                last = mergedInterval;
             }
         }
-        return ans.size();
+        return res;
     }
 }
